41e7b0c4631d56af558945c8a84af8f6a3bc26a0
SM Feroj <sm.feroj@gmail.com>
2023-06-15 16:47:55 +0600

    update

--- index.js
+++ index.js
@@ -8,35 +8,83 @@ require("dotenv").config();
 const morgan = require("morgan");
 const cors = require('cors');
 
-
 // middlewares
-app.use(cors(
-  {origin:["https://deploy-ecom-server-mern-2.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true,
-}
-));
+app.use(cors({
+  origin: ["https://deploy-ecom-server-mern-2.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true,
+}));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(helmet())
-
+app.use(helmet());
 
 // routes middleware
-readdirSync("./routes").map(r => app.use("/api/v1", require(`./routes/${r}`))) 
+readdirSync("./routes").map(r => app.use("/api/v1", require(`./routes/${r}`)));
+
+// Handle root path ("/") request
+app.get('/', (req, res) => {
+  res.send("Hello from the server!");
+});
 
 // server
 const port = process.env.PORT || 7070;
 
 // Connect to DB and start server
 mongoose
-    .connect(process.env.DATABASE)
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server Running on port ${port}`);
-            console.log(`Database connected`);
-        });
-    })
-    .catch((err) => console.log(err));
+  .connect(process.env.DATABASE)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server Running on port ${port}`);
+      console.log(`Database connected`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 module.exports = app;
+
+
+
+
+// const { readdirSync } = require("fs");
+// const path = require("path");
+// const express = require('express');
+// const app = express();
+// const helmet = require('helmet');
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// const morgan = require("morgan");
+// const cors = require('cors');
+
+
+// // middlewares
+// app.use(cors(
+//   {origin:["https://deploy-ecom-server-mern-2.vercel.app"],
+//   methods:["POST","GET"],
+//   credentials:true,
+// }
+// ));
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(helmet())
+
+
+// // routes middleware
+// readdirSync("./routes").map(r => app.use("/api/v1", require(`./routes/${r}`))) 
+
+// // server
+// const port = process.env.PORT || 7070;
+
+// // Connect to DB and start server
+// mongoose
+//     .connect(process.env.DATABASE)
+//     .then(() => {
+//         app.listen(port, () => {
+//             console.log(`Server Running on port ${port}`);
+//             console.log(`Database connected`);
+//         });
+//     })
+//     .catch((err) => console.log(err));
+
+// module.exports = app;
